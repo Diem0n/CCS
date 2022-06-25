5664e7d452820f8f35194fa9b7111bd122f7ffb2
Talha <90450179+Diem0n@users.noreply.github.com>
2022-06-25 12:57:28 +0500

    Add files via upload

---
+++ Css.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.css;
+
+/**
+ *
+ * @author Xer
+ */
+public class Css {
+    public static void main(String[] args) {
+         Home home = new Home();
+         home.setVisible(true);
+        
+    }
+}
